2c5511e23ab90c5d34ec1e2b295f6eac773e25df
Louis Liu <lliu@elta.com.tw>
2022-07-26 09:56:00 +0800

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from playwright.sync_api import sync_playwright
+import re
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto('https://www.facebook.com/login')
+    page.wait_for_load_state('networkidle')
+    html = page.content()
+    html = re.sub('<script[^>]*>(?:.*?)<\/script>', '', html)
+
+    path = 'fb_login.html'
+    f = open(path, 'w')
+    f.write(html)
+    f.close()
+    browser.close()
