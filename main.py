25ffe9304eee5a01106d62e9b6f96de1140e9d2f
Louis Liu <lliu@elta.com.tw>
2022-07-29 11:16:51 +0800

    <fix>: no hacked

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from playwright.sync_api import sync_playwright
 import re
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto('https://www.facebook.com/login')
-    page.wait_for_load_state('networkidle')
-    html = page.content()
-    html = re.sub('<script[^>]*>(?:.*?)<\/script>', '', html)
+    # browser = p.chromium.launch(headless=False)
+    # page = browser.new_page()
+    # page.goto('https://www.facebook.com/login')
+    # page.wait_for_load_state('networkidle')
+    # html = page.content()
+    # html = re.sub('<script[^>]*>(?:.*?)<\/script>', '', html)
 
-    path = 'login.html'
-    f = open(path, 'w')
-    f.write(html)
-    f.close()
-    browser.close()
+    # path = 'login.html'
+    # f = open(path, 'w')
+    # f.write(html)
+    # f.close()
+    # browser.close()
 
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
@@ -24,7 +24,7 @@ with sync_playwright() as p:
     html = page.content()
     html = re.sub('<script[^>]*>(?:.*?)<\/script>', '', html)
 
-    path = 'login_mobile.html'
+    path = 'login.html'
     f = open(path, 'w')
     f.write(html)
     f.close()
